4e9b4e5bf89e8148bd114531e04403b91b011639
Szymon S <58011139+SzymCrusader@users.noreply.github.com>
2022-10-10 23:28:40 +0200

    Modify variable jump

--- Player.gd
+++ Player.gd
@@ -3,14 +3,15 @@ extends CharacterBody2D
 var wasonfloor = true
 
 const SPEED = 300.0
-const JUMP_FORCE = 500
+var JUMP_FORCE = 300
+const MAX_JUMP_FORCE = 500
 @onready var JUMP_BUFFER = $JumpBuffer
 @onready var COYOTE = $CoyoteTime
-const MAX_JUMP = 10000
+const MAX_JUMP = 9000
 @onready var JUMP_DISTANCE = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") *1.5
  
 
 func _physics_process(delta):
@@ -20,12 +21,15 @@ func _physics_process(delta):
 		if Input.is_action_pressed("player_up") and JUMP_DISTANCE<=MAX_JUMP:
 			JUMP_DISTANCE += JUMP_FORCE
 			velocity.y = JUMP_FORCE * (-1)
+			if JUMP_FORCE < MAX_JUMP_FORCE:
+				JUMP_FORCE *= 1.1
 		velocity.y += gravity * delta
 
 	# Handle Jump.
 
 	if (is_on_floor() or COYOTE.time_left>0) and (Input.is_action_just_pressed("player_up") or JUMP_BUFFER.time_left>0):
 		JUMP_DISTANCE = 0
+		JUMP_FORCE = 300
 		velocity.y = JUMP_FORCE * (-1)
 
 		
