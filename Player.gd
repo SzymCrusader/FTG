8c2054446be0352e9d84cc1d80a50d10d49d127e
Szymon S <58011139+SzymCrusader@users.noreply.github.com>
2022-10-10 23:45:40 +0200

    Fix jump buffer

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ const MAX_JUMP_FORCE = 500
 @onready var COYOTE = $CoyoteTime
 const MAX_JUMP = 9000
 @onready var JUMP_DISTANCE = 0
+var JUMP_END = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") *1.5
@@ -17,17 +18,23 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") *1.5
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		print(JUMP_DISTANCE)
-		if Input.is_action_pressed("player_up") and JUMP_DISTANCE<=MAX_JUMP:
+		if Input.is_action_just_pressed("player_up"): 
+			JUMP_BUFFER.start()
+		if Input.is_action_just_released("player_up"):
+			JUMP_END = true
+		if Input.is_action_pressed("player_up") and JUMP_DISTANCE<=MAX_JUMP and not JUMP_END:
 			JUMP_DISTANCE += JUMP_FORCE
 			velocity.y = JUMP_FORCE * (-1)
 			if JUMP_FORCE < MAX_JUMP_FORCE:
 				JUMP_FORCE *= 1.1
 		velocity.y += gravity * delta
 
-	# Handle Jump.
 
+
+	# Handle Jump.
+	print(JUMP_END)
 	if (is_on_floor() or COYOTE.time_left>0) and (Input.is_action_just_pressed("player_up") or JUMP_BUFFER.time_left>0):
+		JUMP_END = false
 		JUMP_DISTANCE = 0
 		JUMP_FORCE = 300
 		velocity.y = JUMP_FORCE * (-1)
