c00a125f22c7fc890b99d10cbb2694f4fcf8614e
Szymon S <58011139+SzymCrusader@users.noreply.github.com>
2022-10-10 14:33:23 +0200

    Add jump buffer and cayote time

--- Player.gd
+++ Player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
+var wasonfloor = true
 
 const SPEED = 300.0
-const JUMP_FORCE = 500
+const JUMP_FORCE = 650
+@onready var JUMP_BUFFER = $Jump_buffer
+@onready var CAYOTE = $CayoteTime
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,9 +15,11 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if Input.is_action_just_pressed("player_jump"): 
+			JUMP_BUFFER.start()
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+	if (is_on_floor() or CAYOTE.time_left>0) and (Input.is_action_just_pressed("player_jump") or JUMP_BUFFER.time_left>0):
 		velocity.y = JUMP_FORCE * (-1)
 
 	# Get the input direction and handle the movement/deceleration.
@@ -24,5 +29,12 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if wasonfloor == false and is_on_floor():
+		wasonfloor = true 
+	
 	move_and_slide()
+	
+	if wasonfloor == true and not is_on_floor():
+		wasonfloor = false
+		CAYOTE.start()
