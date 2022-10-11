7e9740e5a3672731477297761cd3f8e31ae10ffd
Szymon S <58011139+SzymCrusader@users.noreply.github.com>
2022-10-11 23:21:19 +0200

    Make changes to jump

--- Player.gd
+++ Player.gd
@@ -2,48 +2,53 @@ extends CharacterBody2D
 
 var wasonfloor = true
 
-var APEX_BONUS = 0.0
-const SPEED = 300.0
-var JUMP_FORCE = 300
-const MAX_JUMP_FORCE = 500
+@onready var APEX_BONUS = 0.0
+@export var SPEED = 300.0
+@export var JUMP_FORCE = 300
+@export var MAX_JUMP_FORCE = 500
 @onready var JUMP_BUFFER = $JumpBuffer
 @onready var COYOTE = $CoyoteTime
-const MAX_JUMP = 9000
+@export var MAX_JUMP = 9000
 @onready var JUMP_DISTANCE = 0
 var JUMP_END = false
 @onready var APEX_TIMER = $ApexTimer
-var ISAPEX = false
-
+var IS_APEX = false
+@export var GRAVITY_MODIFIER = 4
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") *1.5
- 
+@onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * GRAVITY_MODIFIER
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		if Input.is_action_just_pressed("player_up"): 
 			JUMP_BUFFER.start()
+			
 		if Input.is_action_just_released("player_up"):
-			JUMP_END = true
+			if velocity.y<0:
+				JUMP_END = true
+			
 		if JUMP_DISTANCE >=MAX_JUMP:
 			JUMP_END = true
 			APEX_BONUS = 75.0
-			APEX_TIMER.start()
-		if Input.is_action_pressed("player_up") and JUMP_DISTANCE<=MAX_JUMP and not JUMP_END:
+			if not IS_APEX:
+				APEX_TIMER.start()
+				IS_APEX = true
+				velocity.y = 0
+				
+		if Input.is_action_pressed("player_up") and JUMP_DISTANCE<=MAX_JUMP and not JUMP_END and not IS_APEX:
 			JUMP_DISTANCE += JUMP_FORCE
 			velocity.y = JUMP_FORCE * (-1)
 			if JUMP_FORCE < MAX_JUMP_FORCE:
 				JUMP_FORCE *= 1.5
-		velocity.y += gravity * delta
-
-	if APEX_TIMER.time_left<=0 and APEX_BONUS != 0:
-		APEX_BONUS = 0
-
-	print(velocity.x)
+				
+		if APEX_TIMER.time_left<=0:
+			velocity.y += gravity * delta 
 
 	# Handle Jump.
 
 	if (is_on_floor() or COYOTE.time_left>0) and (Input.is_action_just_pressed("player_up") or JUMP_BUFFER.time_left>0):
+		IS_APEX = false
 		JUMP_END = false
 		JUMP_DISTANCE = 0
 		JUMP_FORCE = 300
