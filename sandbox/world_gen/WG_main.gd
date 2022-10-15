72f4c7662618660cf77ec9058af741e67c683825
Szymon S <58011139+SzymCrusader@users.noreply.github.com>
2022-10-15 14:00:54 +0200

    Fix noise generation density not changing

--- sandbox/world_gen/WG_main.gd
+++ sandbox/world_gen/WG_main.gd
@@ -6,18 +6,17 @@ extends Node2D
 @export var map_height = 0
 @export var iterations = 1
 
-@onready @export var noise_density = 50
+@export var noise_density = 50
 @onready var map = Vector2()
 @onready var map_grid = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rng.randomize()
 	map.x = map_width
 	map.y = map_height 
 	map_grid = make_noise(noise_density)
 	print("noise done")
-	place_tiles(map_grid)
 	map_grid = apply_cellular_automaton(map_grid, iterations)
+	place_tiles(map_grid)
 
 func make_noise(density):
 	var noise_grid = []
@@ -26,15 +25,14 @@ func make_noise(density):
 		for j in map.x:
 			noise_grid[i].append(0)
 	
-	var random = 1
 	
 	for h in map.y:
 		for w in map.x:
-			random = rng.randi_range(1,100)
+			var random = rng.randi_range(1,100)
 			if random > density:
-				noise_grid[h][w] = 1
-			else:
 				noise_grid[h][w] = 0
+			else:
+				noise_grid[h][w] = 1
 	return noise_grid
 func apply_cellular_automaton(grid, count):
 	for i in count:
@@ -65,6 +63,3 @@ func place_tiles(grid):
 		for j in map.x:
 			tiles.set_cell(0, Vector2i(j,i),grid[i][j],Vector2i(0,0))
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("player_up"):
-			place_tiles(map_grid)
